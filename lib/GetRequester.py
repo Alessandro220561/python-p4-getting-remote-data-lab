4d0cb08ca3c252eb6e60012a55081e28fde81047
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-12-18 21:36:42 -0500

    lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        programs_list = []
+        programs = json.loads(self.get_response_body())
+        for program in programs:
+            programs_list.append(program)
+        return programs_list
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+programs_requester = GetRequester(url)
+
+data_sets = programs_requester.load_json()
+
+for data in data_sets:
+    print(data)
